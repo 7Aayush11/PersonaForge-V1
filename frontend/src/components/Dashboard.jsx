6227910315f433dede12082d2a24dfcd2b820ad6
7Aayush11 <nisarayush1172004@gmail.com>
2026-09-23 17:57:26 +0530

    fix: import bug

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import styled, { keyframes } from "styled-components";
 import { supabase } from "../supabaseClient";
 
